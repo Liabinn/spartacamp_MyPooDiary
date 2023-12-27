ff1975b378d21e150d4b36d0cbeac63b312ba683
hyewon <gpdnjs154@navr.com>
2023-12-27 21:29:35 +0900

    프로필 틀 완성, home 머지 전

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,32 @@
+"use client";
+import styled from "styled-components";
+
 const ProfilePage = () => {
   return (
-    <div>
-      <img src="../../../assets/defaultAvatar.JPG" />
-    </div>
+    <Container>
+      <Avatar src="../../../assets/defaultAvatar.JPG" />
+      <h2>화장실 급해요</h2>
+      <p>ddongssaja</p>
+    </Container>
   );
 };
+const Avatar = styled.img`
+  margin: 2.5rem;
+  width: 10rem;
+  border-radius: 50%;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & h2 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+  & p {
+    color: gray;
+  }
+`;
 
 export default ProfilePage;
