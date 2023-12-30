4581799b791d906dcbc9b602cac378ad16618ac9
Liabinn <liabin124@gmail.com>
2023-12-30 15:00:23 +0900

    [MOVE] 로그인-회원가입 파일 나눔

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,86 @@
+'use client'
+import React, { useState, ChangeEvent, FormEvent } from 'react'
+import axios from 'axios'
+import * as St from '@/app/styledComponents/login/StLogin'
+import Spacer from '@/components/ui/Spacer'
+import { useRouter } from 'next/navigation'
+
+
+type userData = {
+  email: string
+  password: string
+}
+
+const Login = () => {
+
+  const router = useRouter();
+
+  // 로그인 시 input 영역 value 가져오기
+  const [loginInputValue, setLoginInputValue] = useState<userData>({
+    email: "",
+    password: "",
+  });
+  const onChangeLoginInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target
+    setLoginInputValue({
+      ...loginInputValue,
+      [name]: value,
+    })
+    console.log(loginInputValue)
+  }
+
+  // 로그인
+  const handleDoLogin = async () => {
+    try {
+      // email과 password를 가져와서 로그인 시도
+      const {data} = await axios.post("http://localhost:4001/login", loginInputValue)
+      const accessToken = data.accessToken;
+      const nickname = data.nickname;
+
+      // 로그인이 완료되면, 토큰을 로컬호스트에 저장(토큰이 없다면 오류)
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("nickname", nickname);
+
+      // 로그인 성공 메시지
+      alert("로그인 성공!");
+
+      // 메인페이지 이동
+      router.push("/");
+
+    } catch (error) {
+      console.log(error)
+      alert("알 수 없는 문제가 발생하였습니다. 잠시 후 다시 시도해 주십시오.")
+    }
+  }
+
+  // 로그인 버튼 클릭 시 submit 막아주고 
+  const handleSubmitOnClick =(e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // db에 저장
+    handleDoLogin();
+  }
+
+  return (
+    <St.SignLoginContainer>
+        <Spacer y={20} />
+        <St.Title>로그인</St.Title>
+        <St.SubTitle>로그인하여 화장실 리뷰와 나만의 일지를 작성해보세요!</St.SubTitle>
+        <Spacer y={20} />
+        <hr />
+        <Spacer y={20} />
+        <St.SectionContainer onSubmit={handleSubmitOnClick}>
+          <St.Section>
+            <St.Label htmlFor='login_id'>ID</St.Label>
+            <St.Input id='login_id' type='email' name='email' onChange={onChangeLoginInput} minLength={4} placeholder='아이디는 4글자 이상 입니다.' />
+          </St.Section>
+          <St.Section>
+            <St.Label htmlFor='login_pw'>PASSWORD</St.Label>
+            <St.Input id='login_pw' type='password' name='password' onChange={onChangeLoginInput} minLength={8} maxLength={20} placeholder='비밀번호는 8~20글자입니다.' />
+          </St.Section>
+        <St.SignLoginBtn type='submit'>로그인</St.SignLoginBtn>
+        </St.SectionContainer>
+      </St.SignLoginContainer>
+  )
+}
+
+export default Login
---
+++ src/components/login/SignUp.tsx
@@ -0,0 +1,99 @@
+'use client'
+import React, { useState, ChangeEvent, FormEvent } from 'react'
+import axios from 'axios'
+import * as St from '@/app/styledComponents/login/StLogin'
+import Spacer from '@/components/ui/Spacer'
+import { useRouter } from 'next/navigation'
+
+type userData = {
+  email: string
+  password: string
+  passwordCheck: string
+  nickname: string
+  check: string
+}
+
+const SignUp = () => {
+
+  const router = useRouter();
+
+  // 회원가입 시 input 영역 value 가져오기
+  const [signUpInputValue, setSignUpInputValue] = useState<userData>({
+    email: "",
+    password: "",
+    passwordCheck: "",
+    nickname: "",
+    check: "",
+  });
+  const onChangeSignUpInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target
+    setSignUpInputValue({
+      ...signUpInputValue,
+      [name]: value,
+    })
+    console.log(signUpInputValue)
+  }
+
+  // 회원가입 시 db에 값 넣어주기
+  const handleDoSignUp = async () => {
+    try {
+      const response = await axios.post("http://localhost:4001/users", signUpInputValue);
+      console.log(response)
+      alert("회원가입 성공! 로그인 페이지로 이동합니다.")
+      router.push("/login");
+    } catch (error) {
+      console.log(error)
+      alert("알 수 없는 문제가 발생하였습니다. 잠시 후 다시 시도해주십시오.")
+    }
+  };
+
+  // 회원가입 버튼 클릭 시 submit 막아주고 
+  const handleSubmitOnClick =(e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // db에 저장
+    handleDoSignUp();
+  }
+
+  return (
+    <St.SignLoginContainer>
+        <Spacer y={20} />
+        <St.Title>회원가입</St.Title>
+        <St.SubTitle>회원가입을 하시면 화장실 리뷰와 나만의 일지를 작성할 수 있습니다!</St.SubTitle>
+        <Spacer y={20} />
+        <hr />
+        <Spacer y={20} />
+        <St.SectionContainer onSubmit={handleSubmitOnClick}>
+          <St.Section>
+            <St.Label htmlFor='id'>ID</St.Label>
+            <St.Input id='id' type='email' onChange={onChangeSignUpInput} minLength={4} name='email' placeholder='아이디는 4글자 이상 입니다.' />
+            <St.InputValueValidation>형식에 맞도록 아이디를 설정해주세요.</St.InputValueValidation>
+          </St.Section>
+          <St.Section>
+            <St.Label htmlFor='pw'>PASSWORD</St.Label>
+            <St.Input id='pw' type='password' onChange={onChangeSignUpInput} minLength={8} maxLength={20} name='password' placeholder='비밀번호는 8~20글자입니다.' />
+            <St.InputValueValidation>형식에 맞도록 비밀번호를 설정해주세요.</St.InputValueValidation>
+          </St.Section>
+          <St.Section>
+            <St.Label htmlFor='pwc'>PASSWORD CHECK</St.Label>
+            <St.Input id='pwc' type='password' onChange={onChangeSignUpInput} minLength={8} maxLength={20} name='passwordCheck' placeholder='비밀번호를 한 번 더 적어주세요.' />
+            <St.InputValueValidation>기존에 적은 비밀번호와 일치하지 않습니다.</St.InputValueValidation>
+          </St.Section>
+          <St.Section>
+            <St.Label htmlFor='nickname'>닉네임</St.Label>
+            <St.Input id='nickname' type='text' onChange={onChangeSignUpInput} minLength={2} maxLength={8} name='nickname' placeholder='닉네임은 2~8글자 한글만 입력가능합니다.' />
+            <St.InputValueValidation>형식에 맞도록 닉네임을 설정해주세요.</St.InputValueValidation>
+          </St.Section>
+          <St.Section>
+            <div className='flex flex-row gap-4'>
+              <St.Label>개인정보 제공 동의</St.Label>
+              <input type='checkbox' onChange={onChangeSignUpInput} name='check' />
+            </div>
+            <St.InputValueValidation>개인정보 제공에 동의하셔야 서비스를 이용하실 수 있습니다.</St.InputValueValidation>
+          </St.Section>
+        <St.SignLoginBtn type='submit'>회원가입</St.SignLoginBtn>
+        </St.SectionContainer>
+      </St.SignLoginContainer>
+  )
+}
+
+export default SignUp
