5cf70f28d75e4ab13896c23dab6627902e2140de
JunHo <wnsgh2098@gmail.com>
2024-01-02 20:00:07 +0900

    맵리스트랑 카카오맵 수정

--- src/components/map/KakaoMap.tsx
+++ src/components/map/KakaoMap.tsx
@@ -1,5 +1,6 @@
 "use client"; //서버에서만 사용가능한 코드를 클라이언트에서도 사용가능하게 해줌
-import React, { useEffect, useState } from "react";
+import { get, mapKeyword, mapKeywordPut, patch, post } from "@/app/api/api";
+import React, { use, useEffect, useState } from "react";
 import {
   Circle,
   Map,
@@ -18,12 +19,17 @@ interface Maker {
   position: { lat: number; lng: number };
   title: string;
 }
-
-const KakaoMap = () => {
+interface KakaoMapProps {
+  keyword1: string;
+  setKeyword: React.Dispatch<React.SetStateAction<string>>;
+}
+const KakaoMap: React.FC<KakaoMapProps> = ({ keyword1, setKeyword }) => {
   const [info, setInfo] = useState<Maker | null>(null);
   const [markers, setMarkers] = useState<Maker[]>([]);
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
-  const [level, setLevel] = useState<number>();
+
+  // const [placeKeyword, setPlaceKeyword] = useState<string>("편의점");
+  // console.log("데이터", placeKeyword.placeKeyword);
   //유저 경로
   const [userLocation, setUserLocation] = useState({
     center: {
@@ -37,7 +43,9 @@ const KakaoMap = () => {
   useEffect(() => {
     if (!map) return;
     const ps = new window.kakao.maps.services.Places();
-
+    const radius = 300; // 반경 300m
+    const keyword = keyword1;
+    console.log("키워드", keyword);
     // 사용자의 위치를 얻습니다.
     navigator.geolocation.getCurrentPosition((position) => {
       const lat = position.coords.latitude;
@@ -51,14 +59,27 @@ const KakaoMap = () => {
         isLoading: false
       }));
       console.log("사용자기준위치", userLocation.center);
+      // 사용자의 위치를 기준으로 반경 300m 내의 장소를 검색합니다.
 
-      // 사용자의 위치를 기준으로 반경 100m 내의 장소를 검색합니다.
-      const radius = 300; // 반경 100m
-      const keyword = " 화장실";
       ps.keywordSearch(
         keyword,
         (data: [{}], status: string) => {
           console.log("데이터 배열", data);
+          data.forEach(async (item: { [key: string]: any }) => {
+            const existingData = await get("maplist");
+
+            if (
+              existingData.some(
+                (data: { [key: string]: any }) => data.id === item.id
+              )
+            ) {
+              // 데이터가 이미 있으면 업데이트
+              await patch(item);
+            } else {
+              // 데이터가 없으면 추가
+              await post(item);
+            }
+          });
           console.log("상태 string", status);
           if (status === window.kakao.maps.services.Status.OK) {
             const bounds = new window.kakao.maps.LatLngBounds();
@@ -103,7 +124,8 @@ const KakaoMap = () => {
         }
       );
     });
-  }, [map]);
+  }, [map, keyword1]);
+
   return (
     <>
       {/* 카카오맵 */}
@@ -113,13 +135,13 @@ const KakaoMap = () => {
           lng: 126.570667
         }}
         style={{ width: "100%", height: "100%" }}
-        level={2}
+        level={4}
         onCreate={setMap}
       >
         {" "}
         <Circle
           center={userLocation.center}
-          radius={250}
+          radius={300}
           strokeWeight={5} // 선의 두께입니다
           strokeColor={"#75B8FA"} // 선의 색깔입니다
           strokeOpacity={1} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
@@ -127,24 +149,31 @@ const KakaoMap = () => {
           fillColor={"#CFE7FF"} // 채우기 색깔입니다
           fillOpacity={0.7} // 채우기 불투명도 입니다
         />
-        <MapMarker position={userLocation.center}>
-          <div style={{ padding: "5px", color: "#000" }}>
-            {"여기가 내 위치야!"}
-          </div>
-        </MapMarker>
         {markers.map((marker) => (
           <MapMarker
             key={`marker-${marker.title}-${marker.position.lat},${marker.position.lng}`}
             position={marker.position}
             onClick={() => setInfo(marker)}
           >
-            {info && info.title === marker.title && (
-              <div style={{ color: "#000" }}>{marker.title}</div>
-            )}
+            <div
+              style={{
+                color: "#000",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap"
+              }}
+            >
+              {marker.title}
+            </div>
           </MapMarker>
         ))}
         <MapTypeControl position={"TOPRIGHT"} />
         <ZoomControl position={"RIGHT"} />
+        <MapMarker position={userLocation.center}>
+          <div style={{ padding: "5px", color: "#1e74ff" }}>
+            {"여기가 내 위치야!"}
+          </div>
+        </MapMarker>
       </Map>
     </>
   );
---
+++ src/components/map/MapLIst.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { get } from "@/app/api/api";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import * as St from "../../app/styledComponents/map/StMap";
+import { usePlaceKeywordMutation } from "@/app/hook/hook";
+interface MapListProps {
+  keyword: string;
+  setKeyword: React.Dispatch<React.SetStateAction<string>>;
+}
+const MapLIst: React.FC<MapListProps> = ({ keyword, setKeyword }) => {
+  const placeKeywordMutation = usePlaceKeywordMutation();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["maplist"],
+    queryFn: () => get("maplist")
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+  // console.log("페이지", data);
+  return (
+    <St.maplistConstainer>
+      <St.maplistTop>맵리스트</St.maplistTop>
+      <St.maplistContentBtnBox>
+        <div onClick={() => setKeyword("편의점")}>편의점</div>
+        <div onClick={() => setKeyword("화장실")}>화장실</div>
+      </St.maplistContentBtnBox>
+      {data
+        ?.filter((item: any) => {
+          if (keyword === "화장실") {
+            return item.category_name === "가정,생활 > 화장실";
+          } else if (keyword === "편의점") {
+            return item.category_group_name === "편의점";
+          } else {
+            return true; // 기본적으로 모든 항목을 포함합니다.
+          }
+        })
+        .map((item: any) => {
+          return (
+            <St.maplistContentBox key={item}>
+              <St.maplistContentBoxTitle>
+                {item.place_name}
+              </St.maplistContentBoxTitle>
+              <St.maplistContentBoxAddress>
+                <p style={{ fontSize: "small" }}>{item.address_name}</p>
+              </St.maplistContentBoxAddress>
+              <St.maplistContentBoxplaceUrl
+                onClick={() => window.open(item.place_url, "_blank")}
+              >
+                <a style={{ fontSize: "small", color: "grey" }} href="">
+                  {" "}
+                  상세보기
+                </a>
+              </St.maplistContentBoxplaceUrl>
+            </St.maplistContentBox>
+          );
+        })}
+    </St.maplistConstainer>
+  );
+};
+
+export default MapLIst;
