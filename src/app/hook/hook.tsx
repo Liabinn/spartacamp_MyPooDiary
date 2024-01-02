7dee79ab09972f086c81eb5873b5abc72495cf5f
JunHo <wnsgh2098@gmail.com>
2024-01-02 19:58:29 +0900

    리액트 쿼리 스크립트 변경

--- src/app/hook/hook.tsx
+++ src/app/hook/hook.tsx
@@ -1,2 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { get, post } from "@/app/api/api";
+import { mapKeywordPut } from "../api/api";
+
+export const usePlaceKeywordMutation = () => {
+  const mutation = useMutation({
+    mutationFn: mapKeywordPut,
+    onSuccess: (data) => {
+      console.log("성공", data);
+    },
+    onError: (error) => {
+      console.log("에러", error);
+    }
+  });
+  return mutation;
+};
