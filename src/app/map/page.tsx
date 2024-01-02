488b2e9509377994665b35e5be0c334828fecbf7
JunHo <wnsgh2098@gmail.com>
2024-01-02 19:59:09 +0900

    맵 페이지 스타일 변경

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,15 +1,25 @@
+"use client";
 import KakaoMap from "@/components/map/KakaoMap";
+import MapLIst from "@/components/map/MapLIst";
 import Script from "next/script";
-import React from "react";
+import React, { useState } from "react";
+import * as St from "../../app/styledComponents/map/StMap";
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&libraries=services,clusterer&autoload=false`;
+
 const MapPage = () => {
+  const [keyword, setKeyword] = useState<string>("편의점");
   return (
-    <div>
+    <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      <main className="flex justify-center items-center w-screen h-screen">
-        <KakaoMap />
-      </main>
-    </div>
+      <St.constainer>
+        <main className="grow  w-1/2 h-lvh">
+          <MapLIst keyword={keyword} setKeyword={setKeyword} />
+        </main>
+        <main className="flex justify-center items-center w-full h-lvh grow">
+          <KakaoMap keyword1={keyword} setKeyword={setKeyword} />
+        </main>
+      </St.constainer>
+    </>
   );
 };
 
