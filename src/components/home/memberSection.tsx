b0026fb40742f9bf8a4ba0d19f74aee9c9934816
jihyun-j <jihyun9142@gmail.com>
2024-01-03 01:34:09 +0900

    [style] 홈페이지 UI 수정

--- src/components/home/memberSection.tsx
+++ src/components/home/memberSection.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {
+  StButton,
+  StMemberOnlyContainer,
+  StMemberOnlyWrapper
+} from "@/app/styledComponents/home/StMemberOnlySection";
 
 const MemberSection = () => {
   return (
-    <div>
-      <Image
-        src="/images/image.jpg"
-        width={300}
-        height={300}
-        alt="이미지"
-      ></Image>
-      <h1>로그인 하시면 나의 쾌변일지를 작성할 수 있습니다.</h1>
-      <Link href="/login"></Link>
-    </div>
+    <StMemberOnlyContainer>
+      <StMemberOnlyWrapper>
+        <Image
+          src="/images/image.png"
+          width={300}
+          height={300}
+          alt="이미지"
+          priority={false}
+        ></Image>
+        <h1>로그인 하시면 나의 쾌변일지를 작성할 수 있습니다.</h1>
+        <Link href="/login">
+          <StButton>로그인</StButton>
+        </Link>
+      </StMemberOnlyWrapper>
+    </StMemberOnlyContainer>
   );
 };
 
